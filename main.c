a6c32e1b6c4d6378e1cf9a3136347f8625d340be
R4ID3NZ4P <iamxtremesifat@gmail.com>
2025-05-20 18:38:16 +0600

    upto binding

--- main.c
+++ main.c
@@ -5,8 +5,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define PORT 8080
+
 int main () {
-    printf("Helo ol. adios...\r\n");
+    int server_filedesc;
+    int host_socket;
+    int client_socket;
+
+    struct sockaddr_in address;
+    int address_length = sizeof(address);
+
+    char buffer[1024] = {0};
+    char *response = "HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\n\r\nHelo. Dis is server in C!";
+
+    host_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(!host_socket) {
+        perror("Socket creation failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; // ip 0.0.0.0 (0x00000000)
+    address.sin_port = htons(PORT);
+
+    if(bind(server_filedesc, (struct sockaddr*) &address, sizeof(address)) < 0) {
+        perror("Binding failed!");
+        exit(EXIT_FAILURE);
+    }
+
 
     return 0;
 }
