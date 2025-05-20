dce8f98a91a83421de43eb8b32ef99d606b814d1
R4ID3NZ4P <iamxtremesifat@gmail.com>
2025-05-20 17:21:06 +0600

    Headers added

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+    printf("Helo ol. adios...\r\n");
+
+    return 0;
+}
